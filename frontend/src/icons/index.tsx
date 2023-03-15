d850c097db4502bb3530cf72af914d88c7c59166
shuvo-me <shuvonaim123@gmail.com>
2023-03-15 22:42:02 +0600

    updated svg icon to react re-usable component

---
+++ frontend/src/icons/index.tsx
@@ -0,0 +1,13 @@
+
+export { default as BranchIcon } from './BranchIcon';
+export { default as CalenderIcon } from './CalenderIcon';
+export { default as CashHandIcon } from './CashHandIcon';
+export { default as DepositIcon } from './DepositIcon';
+export { default as ExpenseIcon } from './ExpenseIcon';
+export { default as FilterIcon } from './FilterIcon';
+export { default as IncomeIcon } from './IncomeIcon';
+export { default as LoanIcon } from './LoanIcon';
+export { default as LoanPlusIcon } from './LoanPlusIcon';
+export { default as PersonPlusIcon } from './PersonPlusIcon';
+export { default as PersonsIcon } from './PersonsIcon';
+export { default as WithdrawIcon } from './WithdrawIcon';
