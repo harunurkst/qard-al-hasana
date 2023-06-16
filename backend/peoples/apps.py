b2628babd67170a352d025a628493c4ce6659ad6
Rahidul Islam <rahiseli@outlook.com>
2023-06-16 20:42:04 +0600

    migration file is removed from .gitignore

--- backend/peoples/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PeoplesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'peoples'
--- backend/peoples/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsSameBranch(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.branch == request.user.staff.branch
