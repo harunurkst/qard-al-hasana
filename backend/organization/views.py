c0381dfd0c5893597b7d2ee2d7eff9c161267566
Rakibul Islam <rakibul.islam7772588@gmail.com>
2023-07-12 12:39:35 +0600

    feat: Added logout api and fixed token expiration

--- backend/organization/views.py
+++ backend/organization/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import get_object_or_404
 from django.db.models import Q, Sum
 from rest_framework import viewsets
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.generics import CreateAPIView, ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
+
 
 from peoples.models import Staff
+
 # from transaction.models import Savings, Loan
 
 from .serializers import (
@@ -17,8 +26,10 @@ from .serializers import (
     MyRefreshSerializer,
     TeamSerializer,
     StaffListSerializer,
-    BranchListSerializer
+    BranchListSerializer,
+    LogoutSerializer,
 )
+
 
 from .models import Team, Branch
 from .paginations.paginations import CommonPageNumberPagination
@@ -27,6 +38,24 @@ from .paginations.paginations import CommonPageNumberPagination
 class LoginView(TokenObtainPairView):
     permission_classes = [AllowAny]
     serializer_class = LoginSerializer
+
+
+class LogoutView(APIView):
+    serializer_class = LogoutSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            refresh = RefreshToken(serializer.validated_data["refresh"])
+            print(refresh)
+            refresh.blacklist()
+        except TokenError:
+            pass
+
+        return Response({"detail": "Successfully logged out."})
 
 
 class RefreshTokenView(TokenRefreshView):
@@ -45,7 +74,7 @@ class RegisterView(CreateAPIView):
 
 
 class TeamCreateListApiView(ListCreateAPIView):
-    queryset = Team.objects.all().order_by('-id')
+    queryset = Team.objects.all().order_by("-id")
     permission_classes = [IsAuthenticated]
     serializer_class = TeamSerializer
     filterset_fields = ["owner", "branch"]
@@ -59,11 +88,11 @@ class TeamRetriveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     serializer_class = TeamDetailSerializer
 
     def get_object(self):
-        return get_object_or_404(Team, id=self.kwargs['pk'])
+        return get_object_or_404(Team, id=self.kwargs["pk"])
 
 
 class StaffReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
-    """ provides only list and retrieve actions """
+    """provides only list and retrieve actions"""
 
     queryset = Staff.objects.all()
     serializer_class = StaffListSerializer
@@ -75,7 +104,7 @@ class StaffReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class BranchReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
-    """ provides only list and retrieve actions """
+    """provides only list and retrieve actions"""
 
     queryset = Branch.objects.all()
     serializer_class = BranchListSerializer
@@ -85,15 +114,19 @@ class BranchReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         # return self.queryset.filter(id=self.request.user.branch.id).annotate(
         return self.queryset.annotate(
-            total_deposit=Sum('basemodel__savings__amount',
-                              filter=Q(basemodel__savings__transaction_type='deposit'), default=0
-                              ) - Sum('basemodel__savings__amount',
-                              filter=Q(basemodel__savings__transaction_type='withdraw'), default=0
-                              ),
-            total_due_loan=Sum(
-                'basemodel__loan__total_due',
-                filter=Q(basemodel__loan__is_paid=False),
-                default=0
+            total_deposit=Sum(
+                "basemodel__savings__amount",
+                filter=Q(basemodel__savings__transaction_type="deposit"),
+                default=0,
             )
+            - Sum(
+                "basemodel__savings__amount",
+                filter=Q(basemodel__savings__transaction_type="withdraw"),
+                default=0,
+            ),
+            total_due_loan=Sum(
+                "basemodel__loan__total_due",
+                filter=Q(basemodel__loan__is_paid=False),
+                default=0,
+            ),
         )
-
