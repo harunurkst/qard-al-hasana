0074ba2abd6f4a28ac5ef8f6d0f44b1c6f512bad
Rahidul Islam <rahiseli@outlook.com>
2023-07-31 14:13:41 +0600

    user branch none issue is fixed

--- backend/organization/serializers.py
+++ backend/organization/serializers.py
@@ -47,10 +47,11 @@ class LoginSerializer(TokenObtainPairSerializer):
         # Add custom claims
         user_data = {
             "username": user.username,
-            "branch": user.branch.id,
+            "branch": user.branch.id if user.branch else None,
             "role": user.role,
         }
         token["user"] = user_data
+        print(token["user"])
 
         return token
 
