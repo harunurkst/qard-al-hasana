f8be5ed290070bcf1675b1a0d88639dd415886c1
Zubair Imtiaz <106450046+ZubairImtiaz3@users.noreply.github.com>
2023-08-23 03:14:07 +0500

    Added Pagination to Member List Page

--- frontend/src/modules/branch/components/MemberTableOfBranch.tsx
+++ frontend/src/modules/branch/components/MemberTableOfBranch.tsx
@@ -4,6 +4,7 @@ import { MemberSavingsType } from '@/types/memberSaving.type';
 import getWeekNumberOfCurrentMonth from '@/utils/getWeekNoOfCurrentMonth';
 import zodSafeQuery from '@/utils/zodSafeQuery';
 import {
+    Button,
     Menu,
     MenuButton,
     MenuItem,
@@ -20,6 +21,7 @@ import { useQuery } from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useMemo, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
 function getStatusBasedOnWeek(baseWeekNo: number, currentWeekNo: number, amount: number) {
     if (amount) return 'DONE';
@@ -41,6 +43,10 @@ interface IMemberTableOfBranch {
 //pdf styling
 
 const IMemberTableOfBranch: React.FC<IMemberTableOfBranch> = ({ teamId, total_members }) => {
+    // States to manage the pagination
+    const [currentPage, setCurrentPage] = useState(0);
+    const resultsPerPage = 10; // or any other number
+
     // const pdfRef = useRef();
     const router = useRouter();
     const [isOpenDepositModal, setOpenDepositModal] = useState(false);
@@ -50,7 +56,37 @@ const IMemberTableOfBranch: React.FC<IMemberTableOfBranch> = ({ teamId, total_me
     // use the hook to fetch member savings
     // const memberTransactions = useMemberSavingsStore((state) => state.memberTransactions);
     const { setTransactions, setSelectedMember } = useMemberSavingsStore((state) => state.actions);
+
     const { data } = useQuery(['memberSaving'], async () => zodSafeQuery(`/api/v1/peoples/members/`)());
+
+    // This function uses the useMemo hook to compute the data items that should be displayed on the current page.
+    const currentResults = useMemo(() => {
+        if (data) {
+            const start = currentPage * resultsPerPage;
+            const end = start + resultsPerPage;
+            return data.result?.results.slice(start, end);
+        }
+        return [];
+    }, [data, currentPage]);
+
+    // This handler is triggered when the user clicks on a page number.
+    const handlePageClick = (selectedItem: { selected: number }) => {
+        setCurrentPage(selectedItem.selected);
+    };
+
+    // Handle advancing to the next page.
+    const handleNext = () => {
+        if (currentPage < Math.ceil(data?.result?.count / resultsPerPage) - 1) {
+            setCurrentPage((prev) => prev + 1);
+        }
+    };
+
+    // Handle going back to the previous page.
+    const handlePrevious = () => {
+        if (currentPage > 0) {
+            setCurrentPage((prev) => prev - 1);
+        }
+    };
 
     setTransactions(data?.result);
     //get total members of a branch
@@ -79,55 +115,117 @@ const IMemberTableOfBranch: React.FC<IMemberTableOfBranch> = ({ teamId, total_me
                         </Tr>
                     </Thead>
                     <Tbody className="text-gray-600">
-                        {data &&
-                            data.result?.results.map((singleData: MemberSavingsType, index) => {
-                                return (
-                                    <Tr key={singleData?.id} className="hover:bg-gray-50">
-                                        <Td>{index + 1}</Td>
-                                        <Td
-                                            onClick={() => router.push(`/member/${singleData.member_id}`)}
-                                            className="cursor-pointer"
-                                        >
-                                            {singleData?.name}
-                                        </Td>
-                                        <Td>{singleData?.guardian_name}</Td>
-                                        <Td>{singleData?.mobile_number}</Td>
-                                        <Td>{singleData?.team}</Td>
-                                        <Td isNumeric> {singleData.balance}</Td>
-                                        <Td isNumeric>
-                                            <Menu>
-                                                <MenuButton
-                                                    className=" inline-flex h-8 w-8  items-center justify-center rounded-full bg-white text-gray-900 hover:border hover:border-gray-200 hover:text-brand-600"
-                                                    as={'button'}
+                        {currentResults.map((singleData: MemberSavingsType, index) => {
+                            return (
+                                <Tr key={singleData?.id} className="hover:bg-gray-50">
+                                    <Td>{index + 1}</Td>
+                                    <Td
+                                        onClick={() => router.push(`/member/${singleData.member_id}`)}
+                                        className="cursor-pointer"
+                                    >
+                                        {singleData?.name}
+                                    </Td>
+                                    <Td>{singleData?.guardian_name}</Td>
+                                    <Td>{singleData?.mobile_number}</Td>
+                                    <Td>{singleData?.team}</Td>
+                                    <Td isNumeric> {singleData.balance}</Td>
+                                    <Td isNumeric>
+                                        <Menu>
+                                            <MenuButton
+                                                className=" inline-flex h-8 w-8  items-center justify-center rounded-full bg-white text-gray-900 hover:border hover:border-gray-200 hover:text-brand-600"
+                                                as={'button'}
+                                            >
+                                                <div className="flex h-full w-full items-center justify-center bg-transparent">
+                                                    <VerticalDotIcon height={16} width={16} stroke="currentColor" />
+                                                </div>
+                                            </MenuButton>
+                                            <MenuList>
+                                                <MenuItem
+                                                    onClick={() => {
+                                                        router.push(`/member/${singleData.member_id}`);
+                                                    }}
                                                 >
-                                                    <div className="flex h-full w-full items-center justify-center bg-transparent">
-                                                        <VerticalDotIcon height={16} width={16} stroke="currentColor" />
-                                                    </div>
-                                                </MenuButton>
-                                                <MenuList>
-                                                    <MenuItem
-                                                        onClick={() => {
-                                                            router.push(`/member/${singleData.member_id}`);
-                                                        }}
-                                                    >
-                                                        View
-                                                    </MenuItem>
-                                                    <MenuItem
-                                                        onClick={() => {
-                                                            setSelectedMember(singleData);
-                                                            setOpenDepositModal(true);
-                                                        }}
-                                                    >
-                                                        সঞ্চয় জমা
-                                                    </MenuItem>
-                                                </MenuList>
-                                            </Menu>
-                                        </Td>
-                                    </Tr>
-                                );
-                            })}
+                                                    View
+                                                </MenuItem>
+                                                <MenuItem
+                                                    onClick={() => {
+                                                        setSelectedMember(singleData);
+                                                        setOpenDepositModal(true);
+                                                    }}
+                                                >
+                                                    সঞ্চয় জমা
+                                                </MenuItem>
+                                            </MenuList>
+                                        </Menu>
+                                    </Td>
+                                </Tr>
+                            );
+                        })}
                     </Tbody>
                 </Table>
+                <div className="flex justify-between px-5 py-4  ">
+                    <Button
+                        onClick={handlePrevious}
+                        leftIcon={
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 20 20"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M15.8332 10.0001H4.1665M4.1665 10.0001L9.99984 15.8334M4.1665 10.0001L9.99984 4.16675"
+                                    stroke="#344054"
+                                    strokeWidth="1.66667"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                            </svg>
+                        }
+                        variant={'outline'}
+                    >
+                        Previous
+                    </Button>
+
+                    <ReactPaginate
+                        previousClassName="hidden"
+                        nextClassName="hidden"
+                        pageCount={Math.ceil(data?.result?.count / resultsPerPage)}
+                        onPageChange={handlePageClick}
+                        pageLinkClassName="h-10 cursor-pointer flex items-center justify-center w-10 text-gray-800 font-medium text-sm rounded-lg hover:bg-gray-100"
+                        activeClassName="bg-gray-200 rounded-lg"
+                        containerClassName="flex items-center"
+                        breakLabel="..."
+                        breakClassName="h-10 flex items-center justify-center px-2 text-gray-800 font-bold text-base"
+                        pageRangeDisplayed={5}
+                        forcePage={currentPage}
+                    />
+
+                    <Button
+                        onClick={handleNext}
+                        rightIcon={
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 20 20"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M4.1665 10.0001H15.8332M15.8332 10.0001L9.99984 4.16675M15.8332 10.0001L9.99984 15.8334"
+                                    stroke="#344054"
+                                    strokeWidth="1.66667"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                            </svg>
+                        }
+                        variant={'outline'}
+                    >
+                        Next
+                    </Button>
+                </div>
             </TableContainer>
         </>
     );
